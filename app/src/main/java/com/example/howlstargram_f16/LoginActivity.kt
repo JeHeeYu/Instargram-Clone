48f1d641583e2194788080b61ca73dee2fc443a7
JeHeeYu <sees1234558@gmail.com>
2022-10-08 22:28:46 +0900

    Add Signin Signup and Login

--- app/src/main/java/com/example/howlstargram_f16/LoginActivity.kt
+++ app/src/main/java/com/example/howlstargram_f16/LoginActivity.kt
@@ -1,11 +1,67 @@
 package com.example.howlstargram_f16
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    var auth: FirebaseAuth? = null
+
+    // 로그인이 성공했을 경우 메인 화면으로 이동
+    fun moveMainPage(user: FirebaseUser?) {
+        if(user != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    // 로그인할 경우
+    fun signinEamil() {
+        auth?.signInWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())?.addOnCompleteListener {
+                task ->
+
+            // ID와 Password가 다 맞았을 경우
+            if(task.isSuccessful) {
+                moveMainPage(task.result?.user)
+            }
+
+            // ID or Password가 틀렸을 경우
+            else {
+                Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun signinAndSignup() {
+        auth?.createUserWithEmailAndPassword(
+            email_edittext.text.toString(),
+            password_edittext.text.toString()
+        )?.addOnCompleteListener { task ->
+
+            // ID가 생성되었을 경우
+            if (task.isSuccessful) {
+                moveMainPage(task.result?.user)
+            }
+            // ID 생성 오류 발생했을 경우
+            else if (task.exception?.message.isNullOrEmpty()) {
+                Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+            }
+            // 회원가입 및 오류가 아닐 경우
+            else {
+                signinEamil()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
+        email_login_button.setOnClickListener {
+            signinAndSignup()
+        }
     }
 }

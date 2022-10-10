04ca0f90b09617cd85f4e80987da82529d076902
JeHeeYu <sees1234558@gmail.com>
2022-10-10 12:43:00 +0900

    Add Google account login

--- app/src/main/java/com/example/howlstargram_f16/LoginActivity.kt
+++ app/src/main/java/com/example/howlstargram_f16/LoginActivity.kt
@@ -4,12 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     var auth: FirebaseAuth? = null
+
+    // Google 로그인 상태 변수
+    var googleSignInClient : GoogleSignInClient? = null
+
+    // Google 로그인 시 사용할 Request Code
+    var GOOGLE_LOGIN_CODE = 9001
 
     // 로그인이 성공했을 경우 메인 화면으로 이동
     fun moveMainPage(user: FirebaseUser?) {
@@ -22,7 +34,6 @@ class LoginActivity : AppCompatActivity() {
     fun signinEamil() {
         auth?.signInWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())?.addOnCompleteListener {
                 task ->
-
             // ID와 Password가 다 맞았을 경우
             if(task.isSuccessful) {
                 moveMainPage(task.result?.user)
@@ -56,6 +67,45 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    // Google 로그인
+    fun googleLogin() {
+        var singInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(singInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    fun firebaseAuthWithGoogle(account : GoogleSignInAccount) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                // ID가 생성되었을 경우
+                if (task.isSuccessful) {
+                    moveMainPage(task.result?.user)
+                }
+                // ID 생성 오류 발생했을 경우
+                else if (task.exception?.message.isNullOrEmpty()) {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+                // 회원가입 및 오류가 아닐 경우
+                else {
+                    signinEamil()
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result.isSuccess) {
+                var account = result.signInAccount
+
+                // 두 번째 단계
+                firebaseAuthWithGoogle(account!!)
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -63,5 +113,17 @@ class LoginActivity : AppCompatActivity() {
         email_login_button.setOnClickListener {
             signinAndSignup()
         }
+
+        google_sign_in_button.setOnClickListener {
+            // 처음 단계
+            googleLogin()
+        }
+
+        var  gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
     }
 }
